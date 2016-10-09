dc715f5644bc3d951d40160e05dfc51ad87de1da
Aditech <aditech5170@gmail.com>
2016-10-09 09:03:17 +0000

    url changed

--- server/config/database.js
+++ server/config/database.js
@@ -2,5 +2,5 @@
  * Created by Aditech on 1/11/2016.
  */
 module.exports = {
-    'url' : 'mongodb://Aditech:adi0100201031@ec2-54-87-209-46.compute-1.amazonaws.com:27017/wontah'
+    'url' : 'mongodb://Aditech:adi0100201031@ec2-52-55-13-110.compute-1.amazonaws.com:27017/wontah'
 };
