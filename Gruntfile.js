a0b87701ffe905bb1e714c0983883cb238ff60e6
Aditech <aditech5170@gmail.com>
2016-10-23 13:53:13 +0000

    changes made 8

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,37 +1,78 @@
+//module.exports = function(grunt) {
+//    //grunt wrapper function
+//    grunt.initConfig({
+//        pkg: grunt.file.readJSON('package.json'),
+//        //grunt task configuration will go here
+//        ngAnnotate: {
+//            options: {
+//                singleQuotes: true
+//            },
+//            app: {
+//                files: {
+//                    './client/custom/scripts/main.min.js': ['./client/custom/scripts/main.js']
+//                }
+//            }
+//        },
+//        concat: {
+//            js: { //target
+//                src: ['./client/custom/scripts/main.min.js'],
+//                dest: './client/custom/scripts/main.min.js'
+//            }
+//        },
+//        uglify: {
+//            js: { //target
+//                src: ['./client/custom/scripts/main.min.js'],
+//                dest: './client/custom/scripts/main.min.js'
+//            }
+//        }
+//    });
+//
+//    //load grunt tasks
+//    grunt.loadNpmTasks('grunt-contrib-concat');
+//    grunt.loadNpmTasks('grunt-contrib-uglify');
+//    grunt.loadNpmTasks('grunt-ng-annotate');
+//
+//    //register grunt default task
+//    grunt.registerTask('default', ['ngAnnotate', 'concat', 'uglify']);
+//};
+// Gruntfile.js
+// our wrapper function (required by grunt and its plugins)
+// all configuration goes inside this function
 module.exports = function(grunt) {
-    //grunt wrapper function
+
+    // CONFIGURE GRUNT ===========================================================
     grunt.initConfig({
+
+        // get the configuration info from package.json ----------------------------
+        // this way we can use things like name and version (pkg.name)
         pkg: grunt.file.readJSON('package.json'),
-        //grunt task configuration will go here
-        ngAnnotate: {
+
+        // configure nodemon
+        nodemon: {
+            dev: {
+                script: 'server.js'
+            }
+        },
+
+        // configure jshint to validate js files -----------------------------------
+        jshint: {
             options: {
-                singleQuotes: true
+                reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
             },
-            app: {
-                files: {
-                    './client/custom/scripts/main.min.js': ['./client/custom/scripts/main.js']
-                }
-            }
-        },
-        concat: {
-            js: { //target
-                src: ['./client/custom/scripts/main.min.js'],
-                dest: './client/custom/scripts/main.min.js'
-            }
-        },
-        uglify: {
-            js: { //target
-                src: ['./client/custom/scripts/main.min.js'],
-                dest: './client/custom/scripts/main.min.js'
-            }
+
+            // when this task is run, lint the Gruntfile and all js files in src
+            build: ['gruntfile.js', 'src']
         }
     });
 
-    //load grunt tasks
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-ng-annotate');
+    // LOAD GRUNT PLUGINS ========================================================
 
-    //register grunt default task
-    grunt.registerTask('default', ['ngAnnotate', 'concat', 'uglify']);
+
+    // we can only load these if they are in our package.json
+
+    // Load nodemon
+    grunt.loadNpmTasks('grunt-nodemon');
+
+    // register the nodemon task when we run grunt
+    grunt.registerTask('default', ['nodemon']);
 };
